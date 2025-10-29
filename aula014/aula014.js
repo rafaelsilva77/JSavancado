7062ba0d3fe0d073cf126b9a34c1bfbf10925027
Rafael dos Santos Silva <githubrafael9@gmail.com>
2025-10-29 19:34:14 -0300

    criei aula 014 e 015

---
+++ aula014/aula014.js
@@ -0,0 +1,19 @@
+let colocação = 15
+
+switch (colocação) {
+    case 1:
+        console.log("Primeiro Lugar")
+        break
+    case 2:
+        console.log("Segundo Lugar")
+        break
+    case 3:
+        console.log("Terceiro Lugar")
+        break
+    case 4: case 5: case 6:
+        console.log("Prêmio de participação")
+    default:
+        console.log("Não subiu ao podio")
+        break
+       
+}
---
+++ aula015/aula015.js
@@ -0,0 +1,11 @@
+console.log("Inicio do programa")
+
+for(let i = 0; i < 10; i++) {
+    if(i % 2 == 0) {
+        console.log(i + " é par")
+    } else {
+        console.log(i + " é impar")
+    }
+}
+
+console.log("Fim do Programa")
