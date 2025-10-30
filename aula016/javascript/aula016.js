b0cc8e546ea454ff3b41b4b6f8bdf151818f5a25
Rafael dos Santos Silva <githubrafael9@gmail.com>
2025-10-29 21:48:35 -0300

    criei aula 016

---
+++ aula016/javascript/aula016.js
@@ -0,0 +1,15 @@
+let num = [10, 20, 30, 40, 50]
+
+for(n of num) {
+    console.log(n)
+}
+
+
+for(n in num) {
+    console.log(num[n])
+} 
+
+for(let i=0; i<num.length;i++) {
+    console.log(num[i])
+} 
+
