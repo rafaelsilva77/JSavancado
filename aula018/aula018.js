8767fc36df1bd03b91975a730aeb793358d91aca
Rafael dos Santos Silva <githubrafael9@gmail.com>
2025-10-30 20:59:34 -0300

    criei aula 018

---
+++ aula018/aula018.js
@@ -0,0 +1,13 @@
+let n = 10
+
+ do{
+    console.log("CFB Cursos")
+    n++
+}while(n < 10)
+console.log("Fim do programa")
+
+/* não executa o while
+while(n < 10) {
+    console.log("CFB Cursos")
+}
+console.log("Fim do programa") */
