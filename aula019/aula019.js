437c8828a0c4b188669a1b82233f0b95e2c89dc6
Rafael dos Santos Silva <githubrafael9@gmail.com>
2025-10-30 21:35:38 -0300

    criei aula 019

---
+++ aula019/aula019.js
@@ -0,0 +1,23 @@
+let n = 0
+let max = 1000
+let pares = 0
+
+for(let i = n; i < max; i++){
+    
+    if(i % 2 !== 0){
+        continue       
+    }
+    pares++
+}
+console.log("Quantidade de pares: " + pares)
+console.log("Fim do programa")
+
+/* o break interrompe o loop e sai do bloco
+while (n < max){
+    console.log("CFB Cursos - " + n)
+    if(n > 10) {
+        break
+    }
+    n++
+}
+console.log("Fim do programa") */
