eeb664b7a474dc3777487b1ce5c2bc125c48bc03
Rafael dos Santos Silva <githubrafael9@gmail.com>
2025-10-31 22:41:07 -0300

    criei aula 022

---
+++ aula022/aula022.js
@@ -0,0 +1,24 @@
+const VALOR_PADRAO = 0
+function add(v) {
+    valor+=v
+}
+let valor = 0
+console.log(valor)
+
+add(10)
+console.log(valor)
+
+add(5)
+console.log(valor)
+
+
+
+
+function soma(n1=VALOR_PADRAO,n2=VALOR_PADRAO) {
+    let res
+    res = n1 + n2
+    return n1 + n2
+}
+
+let resultadfo_soma = soma(5,5)
+//console.log(resultado_soma)
