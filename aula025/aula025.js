130042cb7c540504365af55c1a165b8e31492648
Rafael dos Santos Silva <githubrafael9@gmail.com>
2025-11-03 00:18:04 -0300

    criei aula 025

---
+++ aula025/aula025.js
@@ -0,0 +1,17 @@
+//const soma = function(v1,v2) {return v1+v2}
+
+const soma = (v1,v2)=>{ //funções mais complexas exige o uso de chaves e return
+    let res = v1 + v2
+    return res}
+console.log(soma(10,5))
+
+const nome=n=>{return n}//arrow function
+console.log(nome("Bruno"))
+
+const add=n=>n+10//arrow function simplificada
+console.log(add(10))
+
+
+
+
+
