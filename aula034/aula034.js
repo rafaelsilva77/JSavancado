bb4141dc1611bc5cc995a46e3594999041c14ea2
Rafael dos Santos Silva <githubrafael9@gmail.com>
2025-11-09 22:44:41 -0300

    criei exercício 034

---
+++ aula034/aula034.js
@@ -0,0 +1,25 @@
+//const c1 = document.getElementById("c1")
+//const c1 = document.querySelector("#c1")
+/*const msg =()=> {
+    alert("clicou")
+}   */
+
+const cursos = [...document.querySelectorAll(".curso")]
+
+cursos.map((el)=> {
+    el.addEventListener("click",(evt)=>{
+        const el = evt.target
+        el.classList.add("destaque")
+        console.log(el.innerHTML + " foi clicado")
+    })
+})
+
+
+/* c1.addEventListener("click",(evt)=>{
+    const el = evt.target
+    el.classList.add("destaque")
+})
+ */
+
+
+
