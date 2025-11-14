88e300a53bdb78b619cbd331bfaa498235f692dc
Rafael dos Santos Silva <githubrafael9@gmail.com>
2025-11-14 01:50:41 -0300

    criei aula 037

---
+++ aula037/aula037.js
@@ -0,0 +1,8 @@
+const caixa1 = document.querySelector("#caixa1")
+const btn_c = [...document.querySelectorAll(".curso")]
+
+console.log(document.getRootNode())
+console.log(document.ownerDocument)
+/* console.log(caixa1.firstElementChild)
+console.log(caixa1.lastElementChild)
+console.log(caixa1.children) */
