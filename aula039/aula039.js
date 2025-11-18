d1591828bc473f9e4336cce3a3bef9295bdf0d49
Rafael dos Santos Silva <githubrafael9@gmail.com>
2025-11-18 00:38:18 -0300

    criei aula 039

---
+++ aula039/aula039.js
@@ -0,0 +1,18 @@
+const caixa1 = document.querySelector("#caixa1")
+const btn_c = [...document.querySelectorAll(".curso")]
+const cursos = ["HTML","CSS","JavaScript","PHP","React","MySQL"]
+
+cursos.map((el,chave)=>{
+    const novoElemento = document.createElement("div")
+    novoElemento.setAttribute("id","c" + chave)
+    novoElemento.setAttribute("class","curso c1")
+    novoElemento.innerHTML = el
+    caixa1.appendChild(novoElemento)
+})
+
+
+
+
+
+
+
